d0b7ab7e08ae9833552cbef0bddd9baa6a313b82
@wwwjim <send@jimmy.email>
2021-06-04 01:57:54 -0700

    adds graph to the front page

---
+++ components/Chart.js
@@ -0,0 +1,75 @@
+import * as React from "react";
+import * as d3 from "d3";
+
+const MultilineChart = ({ data = [], dimensions = {} }) => {
+  const svgRef = React.useRef(null);
+  const { width, height, margin = {} } = dimensions;
+  const svgWidth = width + margin.left + margin.right;
+  const svgHeight = height + margin.top + margin.bottom;
+
+  React.useEffect(() => {
+    console.log(data[0].items);
+    const xScale = d3
+      .scaleTime()
+      .domain(d3.extent(data[0].items, (d) => d.date))
+      .range([0, width]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data[0].items, (d) => d.value) + 2000])
+      .range([height, 0]);
+
+    const svgEl = d3.select(svgRef.current);
+    svgEl.selectAll("*").remove();
+
+    const svg = svgEl.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // XAXIS
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%b %d"));
+    const xAxisGroup = svg
+      .append("g")
+      .attr("transform", `translate(0, ${height + 8})`)
+      .call(xAxis);
+    xAxisGroup.select(".domain").remove();
+    xAxisGroup.selectAll("line").attr("stroke", "rgba(255, 255, 255, 0.2)");
+    xAxisGroup
+      .selectAll("text")
+      .attr("opacity", 0.5)
+      .attr("color", "white")
+      .attr("font-size", "12px")
+      .attr("font-family", "Mono")
+      .attr("text-transform", "Uppercase");
+
+    // YAXIS
+    const yAxis = d3.axisLeft(yScale).tickFormat((d) => d);
+    const yAxisGroup = svg.append("g").call(yAxis);
+    yAxisGroup.select(".domain").remove();
+    yAxisGroup.selectAll("line").attr("stroke", "rgba(255, 255, 255, 0.2)");
+    yAxisGroup
+      .selectAll("text")
+      .attr("opacity", 0.5)
+      .attr("color", "white")
+      .attr("font-size", "12px")
+      .attr("font-family", "Mono")
+      .attr("text-transform", "Uppercase");
+
+    const line = d3
+      .line()
+      .x((d) => xScale(d.date))
+      .y((d) => yScale(d.value));
+
+    svg
+      .selectAll(".line")
+      .data(data)
+      .enter()
+      .append("path")
+      .attr("fill", "none")
+      .attr("stroke", (d) => d.color)
+      .attr("stroke-width", 4)
+      .attr("d", (d) => line(d.items));
+  }, [data]);
+
+  return <svg ref={svgRef} width={svgWidth} height={svgHeight} />;
+};
+
+export default MultilineChart;
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import Button from "~/components/Button";
 import FeatureRow from "~/components/FeatureRow";
 import MarketingCube from "~/components/MarketingCube";
 import SingleColumnLayout from "~/components/SingleColumnLayout";
+import Chart from "~/components/Chart";
 
 import { H1, H2, H3, P } from "~/components/Typography";
 import { MarketingUpload, MarketingProgress, MarketingGraph } from "~/components/Marketing";
@@ -24,14 +25,25 @@ export async function getServerSideProps(context) {
   };
 }
 
-function IndexPage(props) {
-  const [state, setState] = React.useState({ miners: [], totalStorage: 0, totalFiles: 0 });
-  React.useEffect(async () => {
-    /*
-    const response = await R.get("/health");
-    console.log(response);
-    */
+function useWindowSize() {
+  const [size, setSize] = React.useState([0, 0]);
+  React.useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+}
 
+function IndexPage(props) {
+  const [width, height] = useWindowSize();
+  const [state, setState] = React.useState({ miners: [], totalStorage: 0, totalFiles: 0 });
+  const [graph, setGraph] = React.useState({ data: null });
+
+  React.useEffect(async () => {
     const miners = await R.get("/public/miners");
     const stats = await R.get("/public/stats");
 
@@ -39,8 +51,78 @@ function IndexPage(props) {
       return setState({ miners: [], totalStorage: 0, totalFiles: 0 });
     }
 
-    setState({ miners, ...stats });
+    setState({ ...state, miners, ...stats });
   }, []);
+
+  React.useEffect(async () => {
+    const load = async () => {
+      const data = await R.get("/public/metrics/deals-on-chain");
+
+      let dealsAttempted = 0;
+      let dealsAttemptedSet = [];
+      let dealsFailed = 0;
+      let dealsFailedSet = [];
+      let dealsOnChain = 0;
+      let dealsOnChainSet = [];
+      let dealsOnChainBytes = 0;
+      let dealsOnChainBytesSet = [];
+      let dealsSealed = 0;
+      let dealsSealedSet = [];
+      let dealsSealedBytes = 0;
+      let dealsSealedBytesSet = [];
+
+      for (let item of data) {
+        dealsAttempted = dealsAttempted + item.dealsAttempted;
+        dealsFailed = dealsFailed + item.dealsFailed;
+        dealsOnChain = dealsOnChain + item.dealsOnChain;
+        dealsOnChainBytes = dealsOnChainBytes + item.dealsOnChainBytes;
+        dealsSealed = dealsSealed + item.dealsSealed;
+        dealsSealedBytes = dealsSealedBytes + item.dealsSealedBytes;
+
+        // TODO(jim): Tell Jeromy this date is annoying
+        if (item.time === "0001-01-01T00:00:00Z") {
+          continue;
+        }
+
+        const date = new Date(item.time);
+
+        dealsAttemptedSet.push({ date, value: dealsAttempted });
+        dealsFailedSet.push({ date, value: dealsFailed });
+        dealsOnChainSet.push({ date, value: dealsOnChain });
+        dealsOnChainBytesSet.push({ date, value: dealsOnChainBytes });
+        dealsSealedSet.push({ date, value: dealsSealed });
+        dealsSealedBytesSet.push({ date, value: dealsSealedBytes });
+      }
+
+      setGraph({
+        dealsSealedBytes,
+        data: [
+          {
+            color: "var(--status-16)",
+            name: "Attempted",
+            items: dealsAttemptedSet,
+          },
+          {
+            color: "var(--status-7)",
+            name: "Failed",
+            items: dealsFailedSet,
+          },
+          {
+            color: "var(--status-6)",
+            name: "OnChain",
+            items: dealsOnChainSet,
+          },
+          {
+            color: "var(--status-success-bright)",
+            name: "Sealed",
+            items: dealsSealedSet,
+          },
+        ],
+      });
+    };
+
+    load();
+  }, [width]);
 
   const description =
     "Use any browser and our API to store public data on the Filecoin Network and retrieve it from anywhere, anytime.";
@@ -78,17 +160,23 @@ function IndexPage(props) {
 
       <div className={S.stats}>
         <div className={S.sc}>
-          <div className={S.scn}>{state.totalFiles}</div>
+          <div className={S.scn}>{state.totalFiles ? state.totalFiles.toLocaleString() : null}</div>
           <div className={S.scl}>Files stored</div>
-        </div>
-        <div className={S.sc}>
-          <div className={S.scn}>{U.bytesToSize(state.totalStorage)}</div>
-          <div className={S.scl}>Total storage</div>
         </div>
         <div className={S.sc}>
           <div className={S.scn}>{state.dealsOnChain}</div>
           <div className={S.scl}>Deals on chain</div>
         </div>
+        <div className={S.sc}>
+          <div className={S.scn}>{U.bytesToSize(state.totalStorage)}</div>
+          <div className={S.scl}>Total storage</div>
+        </div>
+        {graph.dealsSealedBytes ? (
+          <div className={S.sc}>
+            <div className={S.scn}>{U.bytesToSize(graph.dealsSealedBytes)}</div>
+            <div className={S.scl}>Sealed storage</div>
+          </div>
+        ) : null}
       </div>
 
       <SingleColumnLayout style={{ textAlign: "center", marginBottom: 24 }}>
@@ -154,7 +242,41 @@ function IndexPage(props) {
         </P>
       </SingleColumnLayout>
 
+      {graph.data ? (
+        <div className={S.graphArea}>
+          <Chart
+            data={graph.data}
+            dimensions={{
+              width: width - 88,
+              height: 480 + 20,
+              margin: {
+                top: 30,
+                right: 30,
+                bottom: 30,
+                left: 60,
+              },
+            }}
+          />
+        </div>
+      ) : null}
+
       <footer className={S.f}>
+        {graph.data ? (
+          <div className={S.fa}>
+            {graph.data.map((each) => {
+              return (
+                <div className={S.fcol4} key={each.name}>
+                  <div
+                    className={S.graphItem}
+                    style={{ background: each.color, color: `var(--main-text)` }}
+                  >
+                    {each.name}: {each.items[each.items.length - 1].value}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
         <div className={S.fa}>
           <div className={S.fcol4}>
             <span className={S.flink}>Index</span>
