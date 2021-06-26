eb9e1d30b77c07a4e90e6a885b55ba4a0b6b4dd4
@wwwjim <send@jimmy.email>
2021-06-25 17:41:36 -0700

    tries to fix SEO

--- components/Page.tsx
+++ components/Page.tsx
@@ -19,13 +19,13 @@ export default class IndexPage extends React.Component<any> {
           <meta property="og:url" content={url} />
           <meta property="og:title" content={title} />
           <meta property="og:description" content={description} />
-          <meta property="og:image" content="/static/social.png" />
+          <meta property="og:image" content="https://next-s3-public.s3.us-west-2.amazonaws.com/social/filecoin.hd.png" />
 
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content={url} />
           <meta property="twitter:title" content={title} />
           <meta property="twitter:description" content={description} />
-          <meta property="twitter:image" content="/static/social.png" />
+          <meta property="twitter:image" content="https://next-s3-public.s3.us-west-2.amazonaws.com/social/filecoin.hd.png" />
 
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
