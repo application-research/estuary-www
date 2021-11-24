3c31b75385e514379e1a27a5a80380ae293c800c
Marshall Culpepper <marshall@fil.org>
2021-11-24 12:21:14 -0600

    add a CLI retrieval UI with more accurate commands
    this UI fixes the lotus retrieval command when CIDs are
    aggregated (but requires lotus 1.13.2+) by using the
    new `selector` provided in estuary's `by-cid/:cid` API
    endpoint
    
    fixes #17

---
+++ components/RetrievalCommands.tsx
@@ -0,0 +1,45 @@
+import styles from '@components/RetrievalCommands.module.scss';
+import 'react-tabs/style/react-tabs.css';
+
+import * as React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+function RetrievalCommands(props: {
+  miner: string,
+  dealId: string,
+  cid: string,
+  aggregatedIn?: string | null,
+  selector?: string | null,
+}) {
+  const { miner, dealId, cid, aggregatedIn, selector } = props;
+  return (
+    <Tabs selectedTabClassName={styles.selectedTab}>
+      <TabList>
+        {aggregatedIn && (
+          <Tab>lotus 1.13.2+</Tab>
+        )}
+        <Tab>lotus</Tab>
+        <Tab>filc</Tab>
+      </TabList>
+      {aggregatedIn && selector && (
+        <TabPanel>
+          <pre className={styles.command}>
+            lotus client retrieve --miner {miner} --datamodel-path-selector {selector} {aggregatedIn} data-{dealId}
+          </pre>
+        </TabPanel>
+      )}
+      <TabPanel>
+        <pre className={styles.command}>
+          lotus client retrieve --miner {miner} {cid} data-{dealId}
+        </pre>
+      </TabPanel>
+      <TabPanel>
+        <pre className={styles.command}>
+          filc retrieve {cid}
+        </pre>
+      </TabPanel>
+    </Tabs>
+  )
+}
+
+export default RetrievalCommands;
