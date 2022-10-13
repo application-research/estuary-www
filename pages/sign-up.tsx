a68a4a1ccf648d000fc1e2693caa6c41a860a6a7
Stefanie <stefcodes@gmail.com>
2022-10-12 19:13:44 -0700

    fix confirm password logic

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -51,7 +51,7 @@ async function handleRegister(state: any, host) {
   }
 
   // add password confirmation
-  if (U.isValidconfirmPassword(state.confirmPassword) !== state.password) {
+  if (!U.isValidconfirmPassword(state.confirmPassword || state.confirmPassword !== state.password)) {
     return {
       error: 'Passwords do not match',
     };
