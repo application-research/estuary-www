155f7699aa3964a90bf33ca3cde7a3e18c973122
Neel Virdy <neelvirdy@gmail.com>
2022-11-07 13:22:19 -0500

    Positioning

--- components/CreateKeyModalBody.tsx
+++ components/CreateKeyModalBody.tsx
@@ -1,12 +1,11 @@
 import styles from '@pages/app.module.scss';
-import tstyles from '@pages/table.module.scss';
 
 import * as React from 'react';
 import * as R from '@common/requests';
 import Button from './Button';
 import Input from './Input';
-import { useEffect, useState } from 'react';
-import { P } from './Typography';
+import { useState } from 'react';
+import { H4, P } from './Typography';
 import CopyButton from './CopyButton';
 
 function CreateKeyModalBody(props: any) {
@@ -19,37 +18,32 @@ function CreateKeyModalBody(props: any) {
   };
   if (!state.token) {
     return (
-      <React.Fragment>
-        <div className={styles.group} style={{ paddingTop: '16px' }}>
-          <P>Provide a label to associate with the key you are creating.</P>
-          <Input
-            style={{ width: '328px', display: 'inline-block' }}
-            placeholder="Pick something memorable"
-            name="label"
-            onChange={(e) => setState({ ...state, label: e.target.value })}
-            onSubmit={(_) => onSubmit()}
-          />
-          <Button style={{ marginLeft: '16px' }} loading={state.loading ? state.loading : undefined} type="submit" onClick={onSubmit}>
-            Create key
-          </Button>
-        </div>
-      </React.Fragment>
+      <div className={styles.group} style={{ paddingTop: '16px' }}>
+        <P>Provide a label to associate with the key you are creating.</P>
+        <H4 style={{ marginTop: '16px' }}>Label</H4>
+        <Input
+          style={{ marginTop: '8px', width: '416px', display: 'inline-block' }}
+          placeholder="Pick something memorable"
+          name="label"
+          onChange={(e) => setState({ ...state, label: e.target.value })}
+          onSubmit={(_) => onSubmit()}
+        />
+        <Button style={{ marginLeft: '16px' }} loading={state.loading ? state.loading : undefined} type="submit" onClick={onSubmit}>
+          Create key
+        </Button>
+      </div>
     );
   } else {
     return (
-      <React.Fragment>
-        <P>
+      <div className={styles.group} style={{ paddingTop: '16px' }}>
+        <P style={{ maxWidth: '620px' }}>
           Copy and save the following API key corresponding to your "<b>{state.label}</b>" label.
           {/* once db is omitting token, add language: "For security purposes, we do not store this API key. If it is lost or compromised, please revoke
           and create a new key." */}
         </P>
-        <div className={styles.group} style={{ paddingTop: '24px' }}>
-          <P style={{ width: '412px', display: 'inline-block' }}>
-            <b>{state.token}</b>
-          </P>
-          <CopyButton content={state.token} />
-        </div>
-      </React.Fragment>
+        <H4 style={{ marginTop: '16px', width: '488px', display: 'inline-block' }}>{state.token}</H4>
+        <CopyButton content={state.token} />
+      </div>
     );
   }
 }
