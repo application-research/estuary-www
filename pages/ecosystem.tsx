ec58424ed99d8dd423b8414eab707c1afb032d17
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MBP.fios-router.home>
2022-11-25 15:28:32 -0500

    Updated: Alignment on estuary performance page and tweaked mobile responsiveness

--- pages/ecosystem.tsx
+++ pages/ecosystem.tsx
@@ -194,7 +194,6 @@ function EcosystemPage(props: any) {
         <a className={S.ecosystemEstuaryLogo} href="https://estuary.tech/" target="_blank">
           <EstuarySVG height="64px" color="var(--text-white)" />
         </a>
-
         <ul className={S.ecosystemNavList}>
           <li className={S.ecosystemNavListItem}>
             <a className={S.ecosystemNavLink} href="#collaborators">
@@ -214,7 +213,7 @@ function EcosystemPage(props: any) {
         </ul>
       </nav>
 
-      <div className={S.ecosystem} style={{ display: 'grid', rowGap: '80px' }}>
+      <div className={S.ecosystem}>
         <h3 className={S.ecosystemH3} style={{ marginTop: '48px' }}>
           Estuary's performance since April 2021
         </h3>
@@ -227,97 +226,97 @@ function EcosystemPage(props: any) {
           <div style={{ display: 'flex', alignItems: 'flex-start', flexWrap: 'wrap' }}>
             <div className={S.ecosystemLogo}>
               <div className={S.ecosystemLogoBox}>
-                <Logos.Zora className={S.ecosystemImage} />
+                <Logos.Zora height="35px" className={S.ecosystemImage} />
               </div>
             </div>
             <div className={S.ecosystemLogo}>
               <div className={S.ecosystemLogoBox}>
-                <Logos.Portrait className={S.ecosystemImage} />
+                <Logos.Portrait height="35px" className={S.ecosystemImage} />
               </div>
             </div>
             <div className={S.ecosystemLogo}>
               <div className={S.ecosystemLogoBox}>
-                <Logos.NBFS className={S.ecosystemImage} />
+                <Logos.NBFS height="30px" className={S.ecosystemImage} />
               </div>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://archive.org/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/203411654-adf169fb-0493-446a-8393-19d932d93618.png" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/28320272/203411654-adf169fb-0493-446a-8393-19d932d93618.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://kodadot.xyz/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/203411306-01912ea7-9503-4d6a-9501-e243c7123d89.png" />
+                <img className={S.ecosystemImage} height="60vh" src="https://user-images.githubusercontent.com/28320272/203411306-01912ea7-9503-4d6a-9501-e243c7123d89.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://wallet.glif.io/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/203406224-c17a8fd5-fae9-49a0-97c9-3ebf4e704d4f.png" />
+                <img className={S.ecosystemImage} height="80vh" src="https://user-images.githubusercontent.com/28320272/203406224-c17a8fd5-fae9-49a0-97c9-3ebf4e704d4f.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://chainsafe.io/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202939033-a899fadf-5438-44d4-aa09-1c76e660072c.png" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/28320272/202939033-a899fadf-5438-44d4-aa09-1c76e660072c.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://opendata.cityofnewyork.us/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/203404943-0d4d5e2f-195b-4b1e-ab2b-e88fae6a3aac.png" />
+                <img className={S.ecosystemImage} height="80vh" src="https://user-images.githubusercontent.com/28320272/203404943-0d4d5e2f-195b-4b1e-ab2b-e88fae6a3aac.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://app.gala.games/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202942649-b7237e6a-4c38-487a-b167-07a3833917a5.png" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/28320272/202942649-b7237e6a-4c38-487a-b167-07a3833917a5.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://www.vividlabs.com/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/310223/156037345-f93054de-d222-47e9-9653-cd957fc0fcc5.svg" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/310223/156037345-f93054de-d222-47e9-9653-cd957fc0fcc5.svg" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://w3bmint.xyz/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/203404877-791e53c6-7ec6-48b6-960a-f65c4aa46e29.png" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/28320272/203404877-791e53c6-7ec6-48b6-960a-f65c4aa46e29.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://sxxfuture.com/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202938632-e333f117-21a6-4594-95ea-804337a3d9eb.png" />
+                <img className={S.ecosystemImage} height="35vh" src="https://user-images.githubusercontent.com/28320272/204052332-56be823b-b058-4232-96a5-ef3d569dcc56.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://gitopia.com/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202940154-8c54b568-70cd-4063-b21d-38aee052a063.png" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/28320272/202940154-8c54b568-70cd-4063-b21d-38aee052a063.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://hashaxis.com/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202942456-d921ed27-c0c1-4d9e-98ae-f0189e740bc1.svg" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/28320272/202942456-d921ed27-c0c1-4d9e-98ae-f0189e740bc1.svg" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://www.labdao.xyz/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202940852-dda0b5d6-7bb4-4ea3-9c86-ec6bc6286104.svg" />
+                <img className={S.ecosystemImage} height="45vh" src="https://user-images.githubusercontent.com/28320272/202940852-dda0b5d6-7bb4-4ea3-9c86-ec6bc6286104.svg" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://green.filecoin.io/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202937974-6d191fae-264f-40b0-b18e-3071b8009802.png" />
+                <img className={S.ecosystemImage} height="70vh" src="https://user-images.githubusercontent.com/28320272/202937974-6d191fae-264f-40b0-b18e-3071b8009802.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://www.cancerimagingarchive.net/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202939283-c78969dd-2f06-42dd-8823-cb6d23ff3818.png" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/28320272/202939283-c78969dd-2f06-42dd-8823-cb6d23ff3818.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://opsci.io/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202937956-0c12b60d-8a38-4e9b-9749-3420598276f8.png" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/28320272/202937956-0c12b60d-8a38-4e9b-9749-3420598276f8.png" />
               </a>
             </div>
             <div className={S.ecosystemLogo}>
               <a className={S.ecosystemLogoBox} href="https://www.bacalhau.org/" target="_blank">
-                <img className={S.ecosystemImage} src="https://user-images.githubusercontent.com/28320272/202938869-73f5fcc1-7d0c-4e4c-b2d0-bd1d62ceac39.png" />
+                <img className={S.ecosystemImage} height="50vh" src="https://user-images.githubusercontent.com/28320272/202938869-73f5fcc1-7d0c-4e4c-b2d0-bd1d62ceac39.png" />
               </a>
             </div>
           </div>
@@ -430,7 +429,7 @@ function EcosystemPage(props: any) {
           </div>
         ) : null}
 
-        <footer className={S.f}>
+        <div className={S.ecosystemPerformanceTable}>
           <h2 id="deals" className={S.ecosystemH2}>
             Deals
           </h2>
@@ -493,13 +492,16 @@ function EcosystemPage(props: any) {
               </div>
             );
           })}
-        </footer>
+        </div>
       </div>
 
-      <div className={S.fb} style={{ padding: '48px', display: 'grid', gridTemplateColumns: 'repeat(3, minmax(0, 1fr))', paddingLeft: '8vw', paddingRight: '10vw' }}>
+      <footer
+        className={S.fb}
+        style={{ padding: '48px', display: 'grid', gridTemplateColumns: 'repeat(3, minmax(0, 1fr))', paddingLeft: '10vw', paddingRight: '10vw', gridGap: '16px' }}
+      >
         <div className={S.ecosystemHeading}>
-          <a href="https://estuary.tech/" target="_blank" className={S.fcta}>
-            <EstuarySVG height="64px" color="var(--text-white)" style={{ marginLeft: '-1vw' }} />
+          <a href="https://estuary.tech/" target="_blank">
+            <EstuarySVG height="64px" color="var(--text-white)" />
           </a>
         </div>
         <a href="https://arg.protocol.ai" target="_blank" className={S.fcta}>
@@ -508,7 +510,7 @@ function EcosystemPage(props: any) {
         <a href="https://twitter.com/estuary_tech" target="_blank" className={S.fcta}>
           Follow Us
         </a>
-      </div>
+      </footer>
     </Page>
   );
 }
