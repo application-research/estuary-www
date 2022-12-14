f75684a443eba1fc05f2367607a0c877b4a429d5
Neel Virdy <neelvirdy@gmail.com>
2022-12-14 00:11:43 -0500

    add queued status and polish

--- components/PinStatusIcon.tsx
+++ components/PinStatusIcon.tsx
@@ -1,30 +1,36 @@
 import styles from '@components/PinStatusIcon.module.scss';
-import { MoveDownOutlined, PublishedWithChangesOutlined, SyncOutlined, SyncProblemOutlined } from '@mui/icons-material';
+import { HourglassEmptyOutlined, MoveDownOutlined, PublishedWithChangesOutlined, SyncOutlined, SyncProblemOutlined } from '@mui/icons-material';
 import { Tooltip } from '@mui/material';
 
 function PinStatusIcon(props: any) {
-  if (props.pinningStatus == 'pinned') {
+  if (props.pinningStatus == 'queued') {
     return (
-      <Tooltip title={props.pinningStatus}>
-        <PublishedWithChangesOutlined color="success" className={styles.statusIcon} />
+      <Tooltip title={props.pinningStatus} placement="left" arrow>
+        <HourglassEmptyOutlined color="secondary" fontSize="small" className={styles.statusIcon} />
       </Tooltip>
     );
   } else if (props.pinningStatus == 'pinning') {
     return (
-      <Tooltip title={props.pinningStatus}>
-        <SyncOutlined color="primary" className={styles.statusIcon} />
+      <Tooltip title={props.pinningStatus} placement="left" arrow>
+        <SyncOutlined color="primary" fontSize="small" className={styles.statusIcon} />
+      </Tooltip>
+    );
+  } else if (props.pinningStatus == 'pinned') {
+    return (
+      <Tooltip title={props.pinningStatus} placement="left" arrow>
+        <PublishedWithChangesOutlined color="success" fontSize="small" className={styles.statusIcon} />
       </Tooltip>
     );
   } else if (props.pinningStatus == 'failed') {
     return (
-      <Tooltip title={props.pinningStatus}>
-        <SyncProblemOutlined color="error" className={styles.statusIcon} />
+      <Tooltip title={props.pinningStatus} placement="left" arrow>
+        <SyncProblemOutlined color="error" fontSize="small" className={styles.statusIcon} />
       </Tooltip>
     );
   } else if (props.pinningStatus == 'offloaded') {
     return (
-      <Tooltip title={props.pinningStatus}>
-        <MoveDownOutlined color="disabled" className={styles.statusIcon} />
+      <Tooltip title={props.pinningStatus} placement="left" arrow>
+        <MoveDownOutlined color="disabled" fontSize="small" className={styles.statusIcon} />
       </Tooltip>
     );
   }
