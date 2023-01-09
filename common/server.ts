59dba8af4aedce9eaf191134393b0ec1828bf030
Neel Virdy <neelvirdy@gmail.com>
2023-01-09 15:18:29 -0500

    Add PUT and DELETE to cors middleware

--- common/server.ts
+++ common/server.ts
@@ -14,6 +14,6 @@ export function initMiddleware(middleware) {
 
 export const cors = initMiddleware(
   Cors({
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
 );
