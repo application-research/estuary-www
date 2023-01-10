618e915a012e2409d5473c89b0f2a8134640826a
Neel Virdy <neelvirdy@gmail.com>
2023-01-10 15:59:28 -0500

    Remove authenticate with token

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -12,7 +12,7 @@ import Page from '@components/Page';
 import SingleColumnLayout from '@components/SingleColumnLayout';
 import Cookies from 'js-cookie';
 
-import { H2, H3, H4, P } from '@components/Typography';
+import { H2, H4, P } from '@components/Typography';
 
 const ENABLE_SIGN_IN_WITH_FISSION = false;
 
@@ -35,24 +35,6 @@ export async function getServerSideProps(context) {
   };
 }
 
-async function handleTokenAuthenticate(state: any, host) {
-  let response = await fetch(`${host}/user/stats`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${state.key}`,
-    },
-  });
-  if (response && response.status === 403) {
-    alert('Invalid API key');
-  } else if (response && response.status === 401) {
-    alert('Expired API key');
-  } else {
-    Cookies.set(C.auth, state.key);
-    window.location.reload();
-  }
-  return response;
-}
 async function handleSignIn(state: any, host) {
   if (U.isEmpty(state.username)) {
     return { error: 'Please provide a username.' };
@@ -158,40 +140,6 @@ function SignInPage(props: any) {
             href="/sign-up"
           >
             Create an account instead
-          </Button>
-        </div>
-
-        <H3 style={{ marginTop: 32 }}>Authenticate Using Key</H3>
-        <P style={{ marginTop: 8 }}>You can authenticate using an API key if you have one.</P>
-
-        <H4 style={{ marginTop: 32 }}>API key</H4>
-        <Input
-          style={{ marginTop: 8 }}
-          placeholder="ex: ESTxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxARY"
-          name="key"
-          value={state.key}
-          onChange={(e) => setState({ ...state, [e.target.name]: e.target.value.trim() })}
-        />
-
-        <div className={styles.actions}>
-          <Button
-            style={{ width: '100%' }}
-            loading={state.authLoading ? state.authLoading : undefined}
-            onClick={async () => {
-              setState({ ...state, authLoading: true });
-              if (U.isEmpty(state.key)) {
-                alert('Please enter an API key');
-                setState({ ...state, authLoading: false });
-                return;
-              }
-              await handleTokenAuthenticate(state, props.api).then((response) => {
-                if (response.status == 403) {
-                  setState({ ...state, authLoading: false });
-                }
-              });
-            }}
-          >
-            Authenticate
           </Button>
         </div>
       </SingleColumnLayout>
