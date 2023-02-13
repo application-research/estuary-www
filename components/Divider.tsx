1ba2d0dd7edd50b428f543cd08fbf8f362060e66
luc <luc.roy@gmail.com>
2023-02-13 14:14:14 -0400

    added divider component

---
+++ components/Divider.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import styles from '@components/Separator.module.scss';
+
+
+function Separator(props: any) {
+  return (
+    <div className={styles.divider}>Or</div>
+  );
+}
+
+export default Separator;
